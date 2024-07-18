f5c0b1f3024e805ccd54b036b34a1b2c4088ce42
Dongmyeong Lee <domlee@utexas.edu>
2024-07-18 11:28:51 -0500

    add docker

---
+++ run_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DOCKER_IMAGE=domlee93/dataset-tools:latest
+USER=$(whoami)
+DATA_DIR=/media/dongmyeong/T7_Shield/SARA/wilbur
+
+xhost +local:root
+
+# Run the Docker container with X11 forwarding enabled
+docker run -it --rm \
+  --gpus all \
+  --network host \
+  --privileged \
+  --env DISPLAY=$DISPLAY \
+  --env QT_X11_NO_MITSHM=1 \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $(pwd):/home/$USER/dataset-tools \
+  -v $DATA_DIR:/home/$USER/dataset-tools/data/SARA/wilbur \
+  -w /home/$USER/dataset-tools \
+  $DOCKER_IMAGE 
